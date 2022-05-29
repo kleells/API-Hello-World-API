25e38e7d984695634a5bb1971d93d08d51e4f7b1
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-29 17:32:55 -0400

    complete with bonus'

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
+// LANGUAGES
+const languagesController = require('./controllers/languages_controller')
+app.use('/languages', languagesController)
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
